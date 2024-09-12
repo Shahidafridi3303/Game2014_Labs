89835170d8af21e018a2293174c1ea44f4f2242f
Shahid Afridi <115124671+Shahidafridi3303@users.noreply.github.com>
2024-09-12 10:19:13 -0400

    Player Boundaries added

---
+++ Lab-1/Assets/Scripts/Boundaries.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Boundaries
+{
+    public float max, min;
+}
--- Lab-1/Assets/Scripts/PlayerBehaviour.cs
+++ Lab-1/Assets/Scripts/PlayerBehaviour.cs
@@ -6,6 +6,8 @@ public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] float _speed = 3;
 
+    [SerializeField] Boundaries _horizontalBoundary, _verticalBoundary;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +22,23 @@ public class PlayerBehaviour : MonoBehaviour
         float yAxis = Input.GetAxisRaw("Vertical") * _speed * Time.deltaTime;
 
         transform.position += new Vector3(xAxis, yAxis, 0);
+
+        if (transform.position.x > _horizontalBoundary.max)
+        {
+            transform.position = new Vector3(_horizontalBoundary.max, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < _horizontalBoundary.min)
+        {
+            transform.position = new Vector3(_horizontalBoundary.min, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > _verticalBoundary.max)
+        {
+            transform.position = new Vector3(transform.position.x, _verticalBoundary.max, transform.position.z);
+        }
+        else if (transform.position.x < _horizontalBoundary.min)
+        {
+            transform.position = new Vector3(transform.position.x, _verticalBoundary.min, transform.position.z);
+        }
     }
 }

b960bc613e13811b6c565dfa67784ca331eb0af7
Shahid Afridi <115124671+Shahidafridi3303@users.noreply.github.com>
2024-09-25 16:14:52 -0400

    touch input movement and platform recognition added

--- Lab-2/Assets/Scripts/PlayerBehaviour.cs
+++ Lab-2/Assets/Scripts/PlayerBehaviour.cs
@@ -4,27 +4,69 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    [SerializeField] float _speed = 3;
-
+    [SerializeField] float _speed = 3; //changing speed
     [SerializeField] Boundaries _horizontalBoundary, _verticalBoundary;
+    [SerializeField] bool _isTestMobile;
 
+    Camera _camera;
+    Vector2 _destination;
+
+    bool _isMobilePlatform = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _camera = Camera.main;
+
+        if (!_isTestMobile)
+        {
+            _isMobilePlatform = Application.platform == RuntimePlatform.Android ||
+                                Application.platform == RuntimePlatform.IPhonePlayer;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_isMobilePlatform)
+        {
+            GetTouchInput();
+        }
+        else
+        {
+            GetTraditionalInput();
+        }
+
+        Move();
+
+        CheckBoundaries();
+    }
+
+    void Move()
+    {
+        transform.position = _destination;
+    }
+
+    void GetTraditionalInput()
+    {
         // get input and calculate movement amount
         float xAxis = Input.GetAxisRaw("Horizontal") * _speed * Time.deltaTime;
         float yAxis = Input.GetAxisRaw("Vertical") * _speed * Time.deltaTime;
         // Apply movement amount to transform
+        _destination = new Vector3(xAxis + transform.position.x, yAxis + transform.position.y, 0);
+    }
 
-        transform.position += new Vector3(xAxis, yAxis, 0);
+    void GetTouchInput()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            _destination = _camera.ScreenToWorldPoint(touch.position);
+            _destination = Vector2.Lerp(transform.position, _destination, _speed * Time.deltaTime);
+        }
+    }
 
+    void CheckBoundaries()
+    {
         //check if player pass the boundary
         if (transform.position.x > _horizontalBoundary.max)
         {

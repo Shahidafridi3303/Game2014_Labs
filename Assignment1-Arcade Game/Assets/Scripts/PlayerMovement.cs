6c474d0f6d70e28e456cda4c1efb0829f040fdd5
Shahid Afridi <115124671+Shahidafridi3303@users.noreply.github.com>
2024-10-13 17:14:03 -0400

    Added player basic horizontal and vertical movement with boundary limit

---
+++ Assignment1-Arcade Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D _rigidbody;
+    [SerializeField] float _speed = 3; //changing speed
+    [SerializeField] Boundaries _horizontalBoundary, _verticalBoundary;
+    [SerializeField] bool _isTestMobile;
+
+    Camera _camera;
+    Vector2 _destination;
+
+    bool _isMobilePlatform = true;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        _camera = Camera.main;
+
+        if (!_isTestMobile)
+        {
+            _isMobilePlatform = Application.platform == RuntimePlatform.Android ||
+                                Application.platform == RuntimePlatform.IPhonePlayer;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isMobilePlatform)
+        {
+            GetTouchInput();
+        }
+        else
+        {
+            GetTraditionalInput();
+        }
+
+        Move();
+
+        CheckBoundaries();
+    }
+
+    void Move()
+    {
+        transform.position = _destination;
+    }
+
+    void GetTraditionalInput()
+    {
+        // get input and calculate movement amount
+        float xAxis = Input.GetAxisRaw("Horizontal") * _speed * Time.deltaTime;
+        float yAxis = Input.GetAxisRaw("Vertical") * _speed * Time.deltaTime;
+        // Apply movement amount to transform
+        _destination = new Vector3(xAxis + transform.position.x, yAxis + transform.position.y, 0);
+    }
+
+    void GetTouchInput()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            _destination = _camera.ScreenToWorldPoint(touch.position);
+            _destination = Vector2.Lerp(transform.position, _destination, _speed * Time.deltaTime);
+        }
+    }
+
+    void CheckBoundaries()
+    {
+        //check if player pass the boundary
+        if (transform.position.x > _horizontalBoundary.max)
+        {
+            transform.position = new Vector3(_horizontalBoundary.max, transform.position.y, 0);
+        }
+        else if (transform.position.x < _horizontalBoundary.min)
+        {
+            transform.position = new Vector3(_horizontalBoundary.min, transform.position.y, 0);
+        }
+        if (transform.position.y > _verticalBoundary.max)
+        {
+            transform.position = new Vector3(transform.position.x, _verticalBoundary.max, 0);
+        }
+        else if (transform.position.y < _verticalBoundary.min)
+        {
+            transform.position = new Vector3(transform.position.x, _verticalBoundary.min, 0);
+        }
+    }
+}
